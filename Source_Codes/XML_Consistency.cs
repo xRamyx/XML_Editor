816cb480bdff71e21f65b4e8764b6ed49c6130d4
Ramy M.Ahmed <ramy_gaber65@yahoo.com>
2021-12-11 18:05:24 +0200

    Initial commit

---
+++ Source_Codes/XML_Consistency.cs
@@ -0,0 +1,124 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace XML_Editor
+{
+    class XML_Consistency
+    {
+        public int first_time;
+        public string file_name;
+
+        public XML_Consistency(string file_name, int first_time)
+        {
+            this.file_name = file_name;
+            this.first_time = first_time;
+        }
+
+
+        public void output(string s, bool new_line)
+        {
+            //overwrite the file for the first time
+            if (first_time == 0)
+            {
+                StreamWriter file = new StreamWriter(file_name, false);
+                if (new_line == true)
+                {
+                    file.WriteLine(s);
+                    file.Close();
+                }
+                else
+                {
+                    file.Write(s);
+                    file.Close();
+                }
+                first_time = 1;
+            }
+            //append the existing file after the first time
+            else
+            {
+                StreamWriter file = new StreamWriter(file_name, true);
+                if (new_line == true)
+                {
+                    file.WriteLine(s);
+                    file.Close();
+                }
+                else
+                {
+                    file.Write(s);
+                    file.Close();
+                }
+            }
+        }
+
+        private void skipChars(StreamReader input)
+        {
+            char letter = (char)input.Read();
+
+            while (letter != '<')
+            {
+                output(letter.ToString(), false);
+                letter = (char)input.Read();
+            }
+        }
+
+        private char skipSpaces(StreamReader input)
+        {
+            char letter;
+            while (input.Peek() == (int)'\n' || input.Peek() == (int)'\t' || input.Peek() == (int)' ')
+            {
+                letter = (char)input.Read();
+            }
+            return (char)input.Peek();
+        }
+
+        private List<string> readTag(StreamReader input)
+        {
+            char letter = (char)input.Read();
+            List<string> tag_data = new List<string>();
+            tag_data.Add(null);
+            tag_data.Add(null);
+            tag_data.Add(null);
+
+            string tag_name = "";
+
+            
+            while (letter != '>')
+            {
+                tag_name += letter.ToString();
+                letter = (char)input.Read();
+
+                //if there are attributes for the tag
+                if (letter == ' ')
+                {
+                    string attributes = "";
+                    while (letter != '>')
+                    {
+                        attributes += letter.ToString();
+                        letter = (char)input.Read();
+                        //Self-closing tag
+                        if (letter == '/' && input.Peek() == (int)'>')
+                        {
+                            break;
+                        }
+
+                    }
+                    tag_data[2] = attributes;
+                }
+                //Self-closing tag
+                if (letter == '/' && input.Peek() == (int)'>')
+                {
+                    break;
+                }
+            }
+            tag_data[0] = (tag_name == "") ? null : tag_name;
+            tag_data[1] = letter.ToString(); //Self-closing / or >
+            return tag_data;
+        }
+
+        
+    }
+}
