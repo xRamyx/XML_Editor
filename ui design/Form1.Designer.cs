c6f67524fde5b51dddd9898e6864799e8e54b4da
Moamen50 <85317901+Moamen50@users.noreply.github.com>
2021-12-11 19:42:32 +0200

    Add first design of ui

---
+++ ui design/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace ui_design
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.browse = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // browse
+            // 
+            this.browse.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.browse.Font = new System.Drawing.Font("Microsoft JhengHei UI", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.browse.Location = new System.Drawing.Point(728, 442);
+            this.browse.Name = "browse";
+            this.browse.Size = new System.Drawing.Size(132, 37);
+            this.browse.TabIndex = 0;
+            this.browse.Text = "Put XML file";
+            this.browse.UseVisualStyleBackColor = false;
+            this.browse.Click += new System.EventHandler(this.browse_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.textBox1.Location = new System.Drawing.Point(58, 33);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(395, 409);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::ui_design.Properties.Resources.photo_1542903660_eedba2cda473;
+            this.ClientSize = new System.Drawing.Size(886, 516);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.browse);
+            this.Name = "Form1";
+            this.Text = "XML project";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button browse;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ ui design/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ui_design
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void browse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            ofd.Filter = "XML files (*.xml)|*.xml|All files (*.*)|*.*";
+            ofd.ShowDialog();
+            textBox1.Text = File.ReadAllText(ofd.FileName);
+
+        }
+        private void NotChange(KeyPressEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            NotChange(e);
+
+        }
+    }
+}
