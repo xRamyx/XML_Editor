4a769a5964a9f69e437423816b97b1d05356642b
Moamen50 <85317901+Moamen50@users.noreply.github.com>
2021-12-15 19:24:56 +0200

    colour

--- gui design/Form1.Designer.cs
+++ gui design/Form1.Designer.cs
@@ -140,7 +140,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.BackColor = System.Drawing.Color.CadetBlue;
             this.ClientSize = new System.Drawing.Size(1164, 649);
             this.Controls.Add(this.DecompressBtn);
             this.Controls.Add(this.CompressBtn);
