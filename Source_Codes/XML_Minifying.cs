fca6a77d5a4c51f13a5f3bc5790d20659b67e9a6
Rany Refaat Phillip <ranyrefaat@yahoo.com>
2021-12-17 02:38:51 +0200

    Separated classes into files

---
+++ Source_Codes/XML_Minifying.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XML_Formatting
+{
+    class XML_Minifying
+    {
+        private static StreamWriter writer;
+        // Function to set the location of the output file
+        public static void SetLocation(string location)
+        {
+            writer = new StreamWriter(location);
+            writer.AutoFlush = true;
+        }
+        // Function to print the XML file without spaces or new lines
+        public static void Minify(TreeNode node)
+        {
+            writer.Write("<" + node.getTagName() + ">");
+            string value = node.getTagValue();
+            writer.Write(value);
+
+            foreach (TreeNode subtree in node.getChildren())
+            {
+                Minify(subtree);
+            }
+
+            writer.Write("</" + node.getTagName() + ">");
+        }
+    }
+}
